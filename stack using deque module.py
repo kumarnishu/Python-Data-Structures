8c957f437cb64817335142d3389b2d67d689a68f
nishu kumar <kumarnishu437@gmail.com>
2022-12-31 14:47:50 +0530

    arranged in folders

--- stack using deque module.py
+++
@@ -1,9 +0,0 @@
-from collections import deque
-
-stack = deque()
-for i in range(10000000):
-    stack.append(i)
-
-for i in range(10000000):
-    stack.pop()
-print(stack)
--- stack using list.py
+++
@@ -1,6 +0,0 @@
-stack = []
-for i in range(1000):
-    stack.append(i)
-
-for i in range(1000):
-    stack.pop()
